8da34e28040334fc2b17f1a0ea6b2bfe0d78e6d6
ibrahimmi23-meet <109899561+ibrahimmi23-meet@users.noreply.github.com>
2022-07-26 14:14:24 +0300

    hyl

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,17 +1,28 @@
 from flask import Flask, redirect, request, render_template, url_for
 
 
-app = Flask(  magmarang,
+app = Flask(  # Create a flask app
+    __name__,
     template_folder='templates',  # Name of html file folder
     static_folder='static'  # Name of directory for static files
 )
 
-@app.route ('/')
+# Your code should be below
 
-def get_to_the_page():
-    return render_template ("hello.html")
-   
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/product')
+def product ():
+    return render_template('product.html')
+
+
+
 # Your code should be above
 
-if __magmarang == "__main__":  # Makes sure this is the main process
+if __name__ == "__main__":  # Makes sure this is the main process
     app.run(debug=True)
+
+
+
